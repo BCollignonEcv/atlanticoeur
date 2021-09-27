1709afbf6b7a89593aa72d096f4b79493be9f640
BCollignonEcv <baptiste.collignon@mail-ecv.fr>
2021-09-27 03:13:39 +0200

    delete front app folder

---
+++ src/views/Praticiens.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="page">
+    <div class="wrapper-content">
+      <h1>Les praticiens du cabinet <br> SCM Atlanticœur</h1>
+    </div>
+    <doctors-card-list :data-doctors="doctors" :type-card="'big'"/>
+    <doctors-description-list :selected-item="selected" :data-doctors="doctors"/>
+  </div>
+</template>
+
+<script>
+
+// import axios from 'axios'
+import DoctorCards from "@/components/doctor/DoctorCards"
+import DoctorDescriptions from "@/components/doctor/DoctorDescriptions"
+
+export default {
+  name: 'Praticiens',
+  components: {
+    'doctors-card-list': DoctorCards,
+    'doctors-description-list': DoctorDescriptions
+  },
+  data() {
+        return {
+            doctors: [
+                { 
+                  "id": 1, 
+                  "firstName": "Paul", 
+                  "lastName": "Jaillais",
+                  "pathImg": "Paul_Jaillais.png",
+                  "doctolib": "https://partners.doctolib.fr/cardiologue/puilboreau/paul-jaillais?locale=fr",
+                  "specialities": [1,2],
+                  "presentation": "Le docteur Paul Jaillais consulte à la Clinique de l'Atlantique à Puilboreau. Il prend en charge l'ensemble de la pathologie cardiaque mais aussi la cardiologie préventive du sportif, réalise les examens non invasifs, électrocardiogrammes, échographies, épreuves d’effort, enregistrements rythmiques et tensionnels nécessaires aux diagnostics, au traitement et au suivi. Pour la consultation, pensez à prendre vos dernières analyses biologiques et vos éventuels documents cardiologiques antérieurs.",
+                  "vitalCard": "Acceptée",
+                  "tiersPayant": "Sécurité Sociale",
+                  "diplomes": ["Ancien interne","Chef de clinique assistant des hopitaux de Toulouse","DES pathologie cardiovasculaire","DU cardiologie du sport","DU médecines subaquatique"],
+                  "RPPS": 10003840369
+                },
+                { 
+                    "id": 2, 
+                    "firstName": "Michel", 
+                    "lastName": "Fesolowicz",
+                    "pathImg": "Paul_Jaillais.png",
+                    "doctolib": "http://google.fr",
+                    "specialities": [1,2],
+                    "presentation": "Le docteur Paul Jaillais consulte à la Clinique de l'Atlantique à Puilboreau. Il prend en charge l'ensemble de la pathologie cardiaque mais aussi la cardiologie préventive du sportif, réalise les examens non invasifs, électrocardiogrammes, échographies, épreuves d’effort, enregistrements rythmiques et tensionnels nécessaires aux diagnostics, au traitement et au suivi. Pour la consultation, pensez à prendre vos dernières analyses biologiques et vos éventuels documents cardiologiques antérieurs.",
+                    "vitalCard": "Acceptée",
+                    "tiersPayant": "Sécurité Sociale",
+                    "diplomes": ["Ancien interne","Chef de clinique assistant des hopitaux de Toulouse","DES pathologie cardiovasculaire","DU cardiologie du sport","DU médecines subaquatique"],
+                    "RPPS": 10003840369
+                },
+                { 
+                    "id": 3, 
+                    "firstName": "Cyril", 
+                    "lastName": "Huet",
+                    "pathImg": "Paul_Jaillais.png",
+                    "doctolib": "http://google.fr",
+                    "specialities": [1,2],
+                    "presentation": "Le docteur Paul Jaillais consulte à la Clinique de l'Atlantique à Puilboreau. Il prend en charge l'ensemble de la pathologie cardiaque mais aussi la cardiologie préventive du sportif, réalise les examens non invasifs, électrocardiogrammes, échographies, épreuves d’effort, enregistrements rythmiques et tensionnels nécessaires aux diagnostics, au traitement et au suivi. Pour la consultation, pensez à prendre vos dernières analyses biologiques et vos éventuels documents cardiologiques antérieurs.",
+                    "vitalCard": "Acceptée",
+                    "tiersPayant": "Sécurité Sociale",
+                    "diplomes": ["Ancien interne","Chef de clinique assistant des hopitaux de Toulouse","DES pathologie cardiovasculaire","DU cardiologie du sport","DU médecines subaquatique"],
+                    "RPPS": 10003840369
+                },
+                { 
+                    "id": 4, 
+                    "firstName": "Edouard", 
+                    "lastName": "Majou",
+                    "pathImg": "Paul_Jaillais.png",
+                    "doctolib": "http://google.fr",
+                    "specialities": [1,2],
+                    "presentation": "Le docteur Paul Jaillais consulte à la Clinique de l'Atlantique à Puilboreau. Il prend en charge l'ensemble de la pathologie cardiaque mais aussi la cardiologie préventive du sportif, réalise les examens non invasifs, électrocardiogrammes, échographies, épreuves d’effort, enregistrements rythmiques et tensionnels nécessaires aux diagnostics, au traitement et au suivi. Pour la consultation, pensez à prendre vos dernières analyses biologiques et vos éventuels documents cardiologiques antérieurs.",
+                    "vitalCard": "Acceptée",
+                    "tiersPayant": "Sécurité Sociale",
+                    "diplomes": ["Ancien interne","Chef de clinique assistant des hopitaux de Toulouse","DES pathologie cardiovasculaire","DU cardiologie du sport","DU médecines subaquatique"],
+                    "RPPS": 10003840369
+                }
+            ],
+            selected: 1
+        }
+    }
+    // methods: {
+    //     async fetchData(){
+    //         axios.get('http://localhost:3000/doctors').then(res => {
+    //                 axios.get('http://localhost:3000/specialities').then(resSpe => {
+    //                         res.data.forEach((item) => {
+    //                             item['specialities'].forEach( (speciality) => {
+    //                                 item['specialities'][speciality - 1] = resSpe.data[speciality - 1]
+    //                             })
+    //                         })
+    //                         this.doctors = res.data
+    //                     })
+    //             })
+    //     }
+    // },
+    // mounted () {
+    //     this.fetchData()
+    // }
+}
+</script>
+
+<style lang="scss" scoped>
+  .links{
+    p{
+        color: $color-primary;
+    }
+  } 
+</style>
