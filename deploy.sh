de91d72950ee8bb066743f57390c9da56c5aa3c6
BCollignonEcv <baptiste.collignon@mail-ecv.fr>
2021-09-29 03:10:52 +0200

    test deploy

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,8 @@ npm run build
 cd dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
+git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:BCollignonEcv/atlanticoeur.git main:gh-pages
+git push -f git@github.com:BCollignonEcv/atlanticoeur.git master:gh-pages
 cd -
