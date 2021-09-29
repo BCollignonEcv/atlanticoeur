654e1a755d71512376fcc5dd0447fa78ecb24d49
BCollignonEcv <baptiste.collignon@mail-ecv.fr>
2021-09-29 03:09:28 +0200

    deploy

--- deploy.sh
+++ deploy.sh
@@ -9,5 +9,5 @@ cd dist
 # echo 'www.example.com' > CNAME
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:BCollignonEcv/atlanticoeur.git master:gh-pages
+git push -f git@github.com:BCollignonEcv/atlanticoeur.git main:gh-pages
 cd -
