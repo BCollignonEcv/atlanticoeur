1606e0cd102c44be064706a37684ddf0c4e75fa0
BCollignonEcv <baptiste.collignon@mail-ecv.fr>
2021-10-01 15:34:45 +0200

    add destop link page

--- src/components/grid/grid.vue
+++ src/components/grid/grid.vue
@@ -1,23 +1,37 @@
 <template>
-    <div class="grid-line">
-        
+    <div class="wrapper-content">
+        <div class="grid">
+            <grid-item :type-line="'header'" :data-link="links[0].header"/>
+            <div class="grid_content">
+                <grid-item
+                    v-for="(link, index) in links[0].content" :key="index" 
+                    :data-link="link"
+                    :selected="false"
+                />
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 
+import GridItem from '@/components/grid/Grid-item'
 
 export default {
+    components: {
+        'grid-item': GridItem
+    },
     props: {
-        linkData: Object
+        dataLinks: Object
     },
     data() {
         return {
-            link: this.linkData,
+            links: this.dataLinks
         }
     },
     methods: {},
     mounted () {
+        console.log(this.links[0])
     }
 }
 </script>
