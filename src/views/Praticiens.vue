47a5add082a1a435fb928b8163c75cd60a7a3318
BCollignonEcv <baptiste.collignon@mail-ecv.fr>
2021-10-14 03:02:38 +0200

    add specilities caroussel

--- src/views/Praticiens.vue
+++ src/views/Praticiens.vue
@@ -7,6 +7,7 @@
     <doctors-description-list :selected-item="selected" :data-doctors="doctorsSpe"/>
     <div class="wrapper-content">
       <h2>Les spécialités du cabinet <br> SCM Atlanticœur</h2>
+      <specialities-slider :data-specialities="specialities[0]"/>
     </div>
   </div>
 </template>
@@ -15,12 +16,15 @@
 
 import DoctorCards from "@/components/doctor/DoctorCards"
 import DoctorDescriptions from "@/components/doctor/DoctorDescriptions"
+import SpecialitiesSlider from "@/components/speciality/SpecialitiesSlider"
+
 
 export default {
   name: 'Praticiens',
   components: {
     'doctors-card-list': DoctorCards,
-    'doctors-description-list': DoctorDescriptions
+    'doctors-description-list': DoctorDescriptions,
+    'specialities-slider': SpecialitiesSlider
   },
   data() {
         return {
@@ -31,7 +35,10 @@ export default {
                 "lastName": "Jaillais",
                 "pathImg": "Paul_Jaillais.png",
                 "doctolib": "https://partners.doctolib.fr/cardiologue/puilboreau/paul-jaillais?locale=fr",
-                "specialities": [0,1],
+                "specialities": [
+                  ["cardiologie", 0],
+                  ["cardiologie", 1],
+                ],
                 "presentation": "Le docteur Paul Jaillais consulte à la Clinique de l'Atlantique à Puilboreau. Il prend en charge l'ensemble de la pathologie cardiaque mais aussi la cardiologie préventive du sportif, réalise les examens non invasifs, électrocardiogrammes, échographies, épreuves d’effort, enregistrements rythmiques et tensionnels nécessaires aux diagnostics, au traitement et au suivi. Pour la consultation, pensez à prendre vos dernières analyses biologiques et vos éventuels documents cardiologiques antérieurs.",
                 "vitalCard": "Acceptée",
                 "tiersPayant": "Sécurité Sociale",
@@ -44,8 +51,10 @@ export default {
                   "lastName": "Fesolowicz",
                   "pathImg": "Paul_Jaillais.png",
                   "doctolib": "http://google.fr",
-                  "specialities": [2,0],
-                  "presentation": "Le docteur Paul Jaillais consulte à la Clinique de l'Atlantique à Puilboreau. Il prend en charge l'ensemble de la pathologie cardiaque mais aussi la cardiologie préventive du sportif, réalise les examens non invasifs, électrocardiogrammes, échographies, épreuves d’effort, enregistrements rythmiques et tensionnels nécessaires aux diagnostics, au traitement et au suivi. Pour la consultation, pensez à prendre vos dernières analyses biologiques et vos éventuels documents cardiologiques antérieurs.",
+                  "specialities": [
+                    ["angiologie", 0],
+                    ["cardiologie", 1],
+                  ],                  "presentation": "Le docteur Paul Jaillais consulte à la Clinique de l'Atlantique à Puilboreau. Il prend en charge l'ensemble de la pathologie cardiaque mais aussi la cardiologie préventive du sportif, réalise les examens non invasifs, électrocardiogrammes, échographies, épreuves d’effort, enregistrements rythmiques et tensionnels nécessaires aux diagnostics, au traitement et au suivi. Pour la consultation, pensez à prendre vos dernières analyses biologiques et vos éventuels documents cardiologiques antérieurs.",
                   "vitalCard": "Acceptée",
                   "tiersPayant": "Sécurité Sociale",
                   "diplomes": ["Ancien interne","Chef de clinique assistant des hopitaux de Toulouse","DES pathologie cardiovasculaire","DU cardiologie du sport","DU médecines subaquatique"],
@@ -57,7 +66,10 @@ export default {
                   "lastName": "Huet",
                   "pathImg": "Paul_Jaillais.png",
                   "doctolib": "http://google.fr",
-                  "specialities": [0,1],
+                  "specialities": [
+                    ["cardiologie", 0],
+                    ["cardiologie", 1],
+                  ],
                   "presentation": "Le docteur Paul Jaillais consulte à la Clinique de l'Atlantique à Puilboreau. Il prend en charge l'ensemble de la pathologie cardiaque mais aussi la cardiologie préventive du sportif, réalise les examens non invasifs, électrocardiogrammes, échographies, épreuves d’effort, enregistrements rythmiques et tensionnels nécessaires aux diagnostics, au traitement et au suivi. Pour la consultation, pensez à prendre vos dernières analyses biologiques et vos éventuels documents cardiologiques antérieurs.",
                   "vitalCard": "Acceptée",
                   "tiersPayant": "Sécurité Sociale",
@@ -70,7 +82,10 @@ export default {
                   "lastName": "Majou",
                   "pathImg": "Paul_Jaillais.png",
                   "doctolib": "http://google.fr",
-                  "specialities": [0,1],
+                  "specialities": [
+                    ["cardiologie", 0],
+                    ["cardiologie", 1],
+                  ],
                   "presentation": "Le docteur Paul Jaillais consulte à la Clinique de l'Atlantique à Puilboreau. Il prend en charge l'ensemble de la pathologie cardiaque mais aussi la cardiologie préventive du sportif, réalise les examens non invasifs, électrocardiogrammes, échographies, épreuves d’effort, enregistrements rythmiques et tensionnels nécessaires aux diagnostics, au traitement et au suivi. Pour la consultation, pensez à prendre vos dernières analyses biologiques et vos éventuels documents cardiologiques antérieurs.",
                   "vitalCard": "Acceptée",
                   "tiersPayant": "Sécurité Sociale",
@@ -80,21 +95,39 @@ export default {
             ],
             specialities: [
               {
-                "name": "Cardiologie adulte",
-                "description": "hello description"
-              },
-              {
-                "name": "Cardiologie du sport",
-                "description": "hello description"
-              },
-              {
-                "name": "Angiologie",
-                "description": "hello description"
-              },
-              {
-                "name": "Cardiologie adulte",
-                "description": "hello description"
-              },
+                "cardiologie": {
+                  "id": 1,
+                  "name": "Cardiologie",
+                  "pathImg": "/specialities/cardiologie.png",
+                  "discover": "Qu'est ce que la cardiologie",
+                  "subcategory": [
+                    {
+                      "name": "Cardiologie adulte",
+                      "description": "hello description",
+                    },
+                    {
+                      "name": "Cardiologie du sport",
+                      "description": "hello description",
+                    },
+                    {
+                      "name": "Cardiologie adulte",
+                      "description": "hello description",
+                    }
+                  ]
+                },
+                "angiologie": {
+                  "id": 2,
+                  "name": "Angiologie",
+                  "pathImg": "/specialities/angiologie.png",
+                  "discover": "Qu'est ce que l'angiologie",
+                  "subcategory": [
+                    {
+                      "name": "Angiologie",
+                      "description": "hello description",
+                    }
+                  ]
+                },
+              }
             ],
             doctorsSpe: [],
             selected: 1
@@ -104,13 +137,12 @@ export default {
 
        // Change speciality index by content
         includeSpecialities(){
-            this.doctors.forEach((doctor, doctorIndex) => {
+            this.doctors.forEach((doctor) => {
                 Object.entries(doctor.specialities).forEach(entry => {
                     const [key, value] = entry;
-                    doctor.specialities[key] = this.specialities[value]
+                    doctor.specialities[key] = this.specialities[0][value[0]].subcategory[value[1]]
                 });
                 this.doctorsSpe.push(doctor);
-                console.log(doctorIndex, doctor)
             });
         }
     },
